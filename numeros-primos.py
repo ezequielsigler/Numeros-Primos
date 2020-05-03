280095acff53f584cb1b82972fe62b6ba6e41fdb
ezequielsigler <58709126+ezequielsigler@users.noreply.github.com>
2020-05-03 15:09:50 -0300

    script

---
+++ numeros-primos.py
@@ -0,0 +1,23 @@
+
+from time import time
+
+def numerosPrimos(fin = 1000, inicio = 0):
+    tiempo_inicial = time()
+    primos = []
+    for i in range(inicio,fin + 1):
+        if i == 0 or i == 1:
+            pass
+        else:
+            casos_incorrectos = 0
+            for j in range(1,i):
+                n = i%j
+                if n == 0:
+                    casos_incorrectos += 1
+            if casos_incorrectos == 1:
+                primos.append(i)
+    tiempo_final = time()
+    print(primos, "\n\nLa ejecucion tardo {} segundos".format(tiempo_final-tiempo_inicial))
+    
+
+if "__main__" == __name__:
+    numerosPrimos(100000)
